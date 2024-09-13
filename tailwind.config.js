dd6bffbd7190e9fd9bb79034ec57c462abd8c296
cyrusDev1 <cyrushoungue1240@gmail.com>
2024-09-13 20:21:02 +0100

    feat: config tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  "./components/**/*.{js,vue,ts}",
+  "./layouts/**/*.vue",
+  "./pages/**/*.vue",
+  "./plugins/**/*.{js,ts}",
+  "./app.vue",
+  "./error.vue",
+];
+export const theme = {
+  extend: {
+    fontFamily: {},
+    borderRadius: {},
+    colors: {
+      "gray-light": "#CCCCCC",
+      "gray-light-medium": "#D9D9D9",
+      "gray-lighter": "#E6E6E6",
+      "gray-pale": "#F5F5F5",
+      "gray-pearl": "#F7F7F7",
+      "blue-pale": "#ECF4FD",
+      "blue-light": "#F2F7FF",
+      "gray-darkest": "#1A1A1A",
+      "gray-medium": "#808080",
+      blue: "#2F80ED",
+      dark: "#111111",
+    },
   },
-  plugins: [],
+  screens: {
+    sm: "375px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
 };
+export const plugins = [];
