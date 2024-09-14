486cc78f731ce7982834c98b2ad8d0e20e915091
cyrusDev1 <cyrushoungue1240@gmail.com>
2024-09-14 10:36:57 +0100

    feat: add header

---
+++ components/common/landing/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-between items-center">
+    <div>
+      <img src="~/assets/images/logo.svg" alt="Logo" />
+    </div>
+    <div class="py-3 font-inter">
+      <ul class="flex space-x-12">
+        <li v-for="(item, index) in navItems" :key="index">
+          <Link
+            class="text-gray-medium text-xl transition-colors duration-500 hover:text-blue"
+            :to="item.to"
+            >{{ item.text }}</Link
+          >
+        </li>
+      </ul>
+    </div>
+    <div>
+      <Link
+        to="/register"
+        class="text-white font-inter font-medium bg-blue rounded-full text-lg py-4 px-16 transition-opacity duration-500 hover:opacity-80"
+        >Register</Link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Link from "~/components/ui/Link.vue";
+import { ref } from "vue";
+
+const navItems = ref([
+  { text: "Home", to: "/" },
+  { text: "Services", to: "#services" },
+  { text: "Find a Doctor", to: "#find-a-doctor" },
+  { text: "About Us", to: "/#about-us" },
+]);
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="mx-20">
+    <div class="py-8">
+      <Header></Header>
+    </div>
+    <div>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script setup>
+import Header from "~/components/common/landing/Header.vue";
 useHead({
   title: "MedEase",
 });
