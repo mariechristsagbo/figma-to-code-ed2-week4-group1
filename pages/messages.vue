adee2234e37f2ab85d27e9bc383170c8a8d419c6
GODONOU JEAN-HUGUES OLUFEMI <jeanhuguesgodonou@gmail.com>
2024-09-22 17:26:04 +0200

    reprise4

--- pages/messages.vue
+++ pages/messages.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-     <p class="text-customOrange text-2xl  flex  items-center justify-center h-screen">You have no recent messages</p>
+     <p class="text-blue text-2xl  flex  items-center justify-center h-screen">You have no recent messages</p>
     </div>
 </template>
 
