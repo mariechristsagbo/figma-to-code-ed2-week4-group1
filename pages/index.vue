db52924e89f54c6e75abfe59430042da5f49a97e
marcaureldev <marcaurelahouandjinou8@gmail.com>
2024-09-22 13:55:58 +0100

    Conflicts resolution (5)

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,3 @@
 <template>
-    <div class="">
-        <Hero></Hero>
-        <Services></Services>
-        <About></About>
-        <Features></Features>
-        <Testimonials></Testimonials>
-        <Blog></Blog>
-        <Start></Start>
-    </div>
+    <div class=""></div>
 </template>
-<script setup>
-import Hero from "~/components/Hero.vue";
-import Services from "~/components/services/Services.vue";
-import About from "~/components/About.vue";
-import Features from "~/components/features/Features.vue";
-import Testimonials from "~/components/testimonials/Testimonials.vue";
-import Blog from "~/components/blog/Blog.vue";
-import Start from "~/components/Start.vue";
-</script>
